f72c0e33ec0c2808b790304ec4e8cc77c7694b16
jorgew17@gmail.com <jorgew17@gmail.com>
2020-06-03 18:12:16 -0600

    cambio de nombre a rpi01-thing-shadow3-watchdog.sh -> watchdog-rpi01-aws-thing-shadow3.sh

---
+++ watchdog-rpi01-aws-thing-shadow3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ps -ef | grep /home/pi/aws_iot/aws-thing-shadow3.py | grep -v grep
+if [ $? -eq 1 ]; then 
+   date
+   echo "Process not running, restarting /home/pi/aws_iot/aws-thing-shadow3.py"; 
+   python3 /home/pi/aws_iot/aws-thing-shadow3.py -e a3s9lktlbmjbgn-ats.iot.us-east-1.amazonaws.com -r /home/pi/aws_iot/aws-root-ca.pem -c /home/pi/aws_iot/aws-rpi01-iot-certificates/efb09316f4-certificate.pem.crt -k /home/pi/aws_iot/aws-rpi01-iot-certificates/efb09316f4-private.pem.key -n aws-rpi01 -id aws-rpi01 -ri 120 2>&1 | tee /home/pi/aws_iot/aws-thing-shadow3-$(date +%F-%H-%M).log
+fi
