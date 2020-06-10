6ae83e54820ff75f784d1025d6e9f30a44a35bf6
Jorge Alvarado <jorgew17@gmail.com>
2020-06-09 19:45:08 -0600

    implementado serial que lee nextion display

--- serial-read.py
+++ serial-read.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 import time
 import serial
 from Atlas import atlas_i2c
@@ -13,22 +14,33 @@ ser = serial.Serial(
         timeout=1
 )
 counter=0
-ser.write(b't1.txt=\"test\"\xff\xff\xff')
-ser.write(b't0.bco=63488\xff\xff\xff')
+#ser.write(b't1.txt=\"test\"\xff\xff\xff')
+#ser.write(b't0.bco=63488\xff\xff\xff')  
+
+
+
 while 1:
     #ser.write(b't0.txt=\"test\"\xff\xff\xff')
     #ser.write(b'get t0.txt\xff\xff\xff')
     #ser.write(b'sendme\xff\xff\xff')
+    
+    with open('/home/pi/aws_iot/rpi-cpu-core-temp.log','r') as f:
+      core_temp_json = json.load(f)
+      f.close()
+      print(core_temp_json['CPUTemp'])
+      #cputemp= 't2.txt=\"' + str(core_temp_json['CPUTemp']) + '\"\xff\xff\xff'
+      ser.write(b't2.txt=\"' + str.encode(repr(core_temp_json['CPUTemp'])) + b'\"\xff\xff\xff')
+      time.sleep(0.005)
+      
+      
     x = ser.readline()
     #print(type(x))
     #print(len(x))
     if (len(x) > 0):
       if (x[0:1] == b'e'):
         print("~~~ evento ~~~")
-        if (x[2:3] == b'\x0b'):
+        if (x[2:3] == b'\x05'):
           print("purge button")
-        elif(x[2:3] == b'\x08'):
-          print("lights button")
         elif(x[2:3] == b'\x01'):
           print("read ec button")
           ec = device.query('r')
