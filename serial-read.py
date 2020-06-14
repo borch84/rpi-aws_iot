39101560f3c57070f8a7f571b363fd7687d04e45
Jorge Alvarado <jorgew17@gmail.com>
2020-06-14 16:20:34 -0600

    timer en serial-read con threading

--- serial-read.py
+++ serial-read.py
@@ -25,13 +25,29 @@ x = ser.readline()
 value = x[1:2]
 print(value)  """
 
-def purge_stop(timeout_seconds,pump_pin,sole_pin,ser):
-  #GPIO.output(sole_pin,0) # 0 signal value activates relay pin            
-  #GPIO.output(pump_pin,0) # 0 signal value activates relay pin
-  #time.sleep(int(timeout_seconds))
-  GPIO.output(sole_pin,1)
-  GPIO.output(pump_pin,1)
-  ser.write(b'purge_button.val=0\xff\xff\xff')
+e = threading.Event()
+doRun = False
+
+def purge_start(timeout_seconds,pump_pin,sole_pin,ser):
+  #global doRun
+  while True:
+    print("Thread esperando hasta que usuario active boton de purga")
+    e.wait()
+    GPIO.output(sole_pin,0) # 0 signal value activates relay pin            
+    GPIO.output(pump_pin,0) # 0 signal value activates relay pin
+    #time.sleep(int(timeout_seconds))
+    #GPIO.output(sole_pin,1)
+    #GPIO.output(pump_pin,1)
+    #ser.write(b'purge_button.val=0\xff\xff\xff')
+
+def purge_timer(pump_pin,sole_pin,ser):
+    #global doRun
+    #doRun = False
+
+    #e.wait()
+    GPIO.output(sole_pin,1)
+    GPIO.output(pump_pin,1)
+    ser.write(b'purge_button.val=0\xff\xff\xff')
 
 def purge_cancel(pump_pin,sole_pin,ser):
   GPIO.output(sole_pin,1)
@@ -42,7 +58,7 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 sole_pin = 4
 pump_pin = 17
-timeout_seconds = 10
+timeout_seconds = 5
 # Cada vez que el programa inicia hay que desactivar la purga
 ser.write(b'purge_button.val=0\xff\xff\xff')
 purgeOff = True
@@ -52,7 +68,10 @@ GPIO.output(sole_pin,1) # 1 desactiva el relay
 GPIO.output(pump_pin,1) 
 
 
-timer = threading.Timer(timeout_seconds,purge_stop,args=(timeout_seconds,pump_pin,sole_pin,ser))
+#timer = threading.Timer(timeout_seconds,purge_timer,args=(pump_pin,sole_pin,ser))
+
+thread_purge = threading.Thread(target=purge_start, args=(timeout_seconds,pump_pin,sole_pin,ser))
+thread_purge.start()
 
 while 1:
     #ser.write(b't0.txt=\"test\"\xff\xff\xff')
@@ -66,14 +85,15 @@ while 1:
         #print(core_temp_json['CPUTemp'])
         #cputemp= 't2.txt=\"' + str(core_temp_json['CPUTemp']) + '\"\xff\xff\xff'
         ser.write(b't2.txt=\"' + str.encode(repr(core_temp_json['CPUTemp'])) + b'\"\xff\xff\xff')
-        time.sleep(0.005)
+        #time.sleep(0.005)
     except Exception as e:
       print("!!! serial-read.py exception: ",repr(e))
       
+    
     x = ser.readline()
     #print(type(x))
     #print(len(x))
-
+    timer = threading.Timer(timeout_seconds,purge_timer,args=(pump_pin,sole_pin,ser))    
     
 
     if (len(x) > 0):
@@ -85,24 +105,41 @@ while 1:
           x = ser.readline()
           value = x[1:2]
           if value == b'\x01':
-            #thread_purge = threading.Thread(target=purge_start, args=(timeout_seconds,pump_pin,sole_pin,ser))
-            #thread_purge.start()
+            #Activa el boton. 
+            #current_thread = threading.current_thread()
+            #if not current_thread.is_alive():
+            #if not doRun: 
+            #  doRun = True
+            #  timer.start()
+            #if threading.enumerate() == 1:
             GPIO.output(sole_pin,0) # 0 signal value activates relay pin            
             GPIO.output(pump_pin,0) # 0 signal value activates relay pin
             timer.start()
+            #e.set()
+            #e.clear()
+            #thread_purge.join()
           elif value == b'\x00':
+            #Apaga el boton.
             #thread_purge_cancel = threading.Thread(target=purge_cancel, args=(pump_pin,sole_pin,ser))
             #thread_purge_cancel.start()
             #for th in threading.enumerate():
               #print(th.get_native_id())
-              #th.do_run = False
+              #th.cancel()
 
             #current_thread = threading.currentThread()
             #current_thread.do_run = False
-            timer.cancel()
+            #if current_thread.is_alive():
+          
+            #if doRun:
+            #  doRun = False
+            #  timer.cancel()
+            
             GPIO.output(sole_pin,1) # 1 desactiva el relay 
             GPIO.output(pump_pin,1) 
             ser.write(b'purge_button.val=0\xff\xff\xff')
+            timer.cancel()
+            #e.wait()
+
 
         elif(x[2:3] == b'\x01'):
           print("read ec button")
