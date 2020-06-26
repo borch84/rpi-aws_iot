f8c6d27a1db736741723a0822906d93178705b16
Jorge Alvarado <jorgew17@gmail.com>
2020-06-26 17:10:35 -0600

    remove second parameter, add purge time json path parameter

--- ec100.py
+++ ec100.py
@@ -2,7 +2,7 @@
 from __future__ import print_function
 import time
 import sys 
-
+import time_purge_handler_file
 import argparse
 import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
@@ -30,16 +30,19 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-sp", "--solenoide_pin",action="store", required=True, dest="solepin", help="BCM Solenoide Pin Relay Number")
     parser.add_argument("-pp", "--pump_pin",action="store", required=True, dest="pumppin", help="BCM Pump Pin Relay Number")
-    parser.add_argument("-s", "--seconds", action="store", required=True, dest="seconds", help="Number of seconds the pin should be On")
     parser.add_argument("-maxec", "--maxec",action="store", required=True, dest="maxec", help="Upper EC Value")
     parser.add_argument("-fp", "--file_path",action="store", required=True, dest="file_path", help="EC Reading JSON File Path")
+    parser.add_argument("-pt", "--purge_time",action="store", required=True, dest="purge_time", help="Purge Time JSON File Path")
+
 
     args = parser.parse_args()
     sole_pin = int(args.solepin)
     pump_pin = int(args.pumppin)
-    timeout_seconds = int(args.seconds)
     maxec = int(args.maxec)
     file_path = args.file_path
+    purge_time_fp = args.purge_time
+
+    timeout_seconds = time_purge_handler_file.read_purge_time_json('/home/pi/rpi-aws_iot/purge_time.json')
 
     ec_device = atlas_i2c(address=100,bus=1,file_path=file_path)  # creates the I2C port object, specify the address, bus and file path name to write the json object
     mylcd = RPI_I2C_driver.lcd()
