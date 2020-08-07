b2a1f1fa9aca1aecdefb4e1ea9fec717decfd791
borch84 <jorgew17@gmail.com>
2020-08-06 19:29:54 -0600

    update insert-influxdb.py

---
+++ fs_usage.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+fs_usage=`df -h / | tail -1 | awk '{print $5}'| sed 's/\%//'`
+echo "{\"fs_usage\":$fs_usage}" > fs_usage.json
+cat fs_usage.json
--- insert-influxdb.py
+++ insert-influxdb.py
@@ -1,27 +1,56 @@
 #!/usr/bin/python3
 import json
+import argparse
 from influxdb import InfluxDBClient
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file",action="store", required=True, dest="file", help="Measurement JSON File")
+parser.add_argument("-m", "--measurement",action="store", required=True, dest="measurement", help="InfluxDB Measurement")
+parser.add_argument("-s", "--sensor_id",action="store", required=True, dest="sensor_id", help="Sensor ID")
+parser.add_argument("-l", "--label",action="store", required=True, dest="label", help="Field label name in the measurement")  
+
+args = parser.parse_args()
+file_path = args.file
+measurement = args.measurement
+sensor_id = args.sensor_id
+label = args.label
 
 client = InfluxDBClient(host='localhost',port=8086)
 client.switch_database('iotdb')
 try:
-    with open("/home/pi/aws_iot/ph99.json","r") as ph_file:
-        ph_data = ph_file.read()
-    ph_file.close()
-    ph_json = json.loads(ph_data)
-    ph_JSONPayload = ""
-    if ph_json["status"] == "OK":
-      json_body = [
-        {
-        "measurement": "ph99",
-        "fields":
-            {
-              "id": "99",
-              "ph": ph_json["ph"]
-            }
-        }
-      ]
-      client.write_points(json_body)
-      client.close()
+    with open(file_path,"r") as file:
+        data = file.read()
+    file.close()
+    json_data = json.loads(data)
+    if not (json_data.get("status") is None):
+      if json_data["status"] == "OK":
+        json_body = [
+          {
+          "measurement": measurement,
+          "fields":
+              {
+                "id": sensor_id,
+                label: json_data[label]
+              }
+          }
+        ]
+        client.write_points(json_body)
+        client.close()
+        print("write OK")
+    else:
+        json_body = [
+          {
+          "measurement": measurement,
+          "fields":
+              {
+                "id": sensor_id,
+                label: json_data[label]
+              }
+          }
+        ]
+        client.write_points(json_body)
+        client.close()
+        print("write OK")
+
 except Exception as e:
-    print ("Ph Exception: "+repr(e))
+    print ("Exception: "+repr(e))
