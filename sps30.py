81c6ee85d3f1098e8b742d56435a2b2df1c00d51
borch84 <jorgew17@gmail.com>
2021-01-06 01:01:00 -0600

    update sps30 json path

--- sps30.py
+++ sps30.py
@@ -3,7 +3,7 @@ def jsonpayload():
   #sps30_JSONPayload = ""
   #Abrir el archivo para leer la informacion del SPS30
   try:
-    f = open('/home/pi/aws_iot/Sensirion/sps30-uart-3.0.0/sps30.json', 'r') 
+    f = open('/home/pi/aws_iot/sps30.json', 'r') 
     sps30_JSONPayload = f.read()
     sps30_JSONPayload = '\"sps30\": ' + sps30_JSONPayload
     f.close()
